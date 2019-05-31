e10ed50268fa6896a5ecf9509e9320cfaeb0a81a
Ranit Dey <you@example.com>
2019-05-31 10:48:25 +0530

    added unit tests

--- src/it/java/com/n26/controller/StatisticsControllerTest.java
+++ src/it/java/com/n26/controller/StatisticsControllerTest.java
@@ -1,6 +1,7 @@
 package unit.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.n26.Application;
 import com.n26.controllers.StatisticsController;
 import com.n26.entities.Statistics;
 import com.n26.services.StatisticsService;
@@ -11,6 +12,7 @@ import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
